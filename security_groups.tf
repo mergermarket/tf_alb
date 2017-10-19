da23c937d133e1aa8c35ff03033d537603b1a0a5
Bart Swedrowski <bart@timedout.org>
2017-10-19 16:03:37 +0100

    Allow traffic to port 80
    Some of the ALBs we're deploying will have something listening on port
    80

--- security_groups.tf
+++ security_groups.tf
@@ -2,6 +2,12 @@ resource "aws_security_group" "default" {
   vpc_id = "${var.vpc_id}"
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
